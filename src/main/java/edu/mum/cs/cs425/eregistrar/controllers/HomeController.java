492e712db37444ef1f765d4e8fd6ebc33e48a12b
Nduwayo Fabrice <nduwayof@outlook.com>
2019-07-31 00:07:11 -0500

    Initialized the git repository

---
+++ src/main/java/edu/mum/cs/cs425/eregistrar/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package edu.mum.cs.cs425.eregistrar.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(path = {"/",
+            "/home", "/eregistrar",
+            "/eregistrar/home"})
+    public String home() {
+        return "index";
+    }
+
+}
